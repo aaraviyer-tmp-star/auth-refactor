772359b5ddccfafb7a89252b88cc3c8595907cb6
aaraviyer-tmp-star <aarav.iyer-tmp@turing.com>
2026-05-11 10:21:47 +0100

    chore: add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts', 'tests/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'coverage/'],
+  },
+];
